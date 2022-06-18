f3d343c920b7cd7b1acf8e471c4a4a0fd80b50ca
golevArtemOhta <golevartem1995@gmail.com>
2022-06-18 21:22:54 +0300

    add info fragment

--- app/src/main/java/com/example/wildberriesweek71/MainActivity.kt
+++ app/src/main/java/com/example/wildberriesweek71/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.wildberriesweek71
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.wildberriesweek71.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +15,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            openFragment(DotaHeroesFragment.newInstance())
+            openFragment(DotaHeroesFragment.newInstance(), null)
         }
 
         binding.imageButtonInfo.setOnClickListener {
-            openFragment(InfoFragment.newInstance())
+            openFragment(InfoFragment.newInstance(), "stack")
         }
     }
 
-    private fun openFragment(fragment: Fragment) {
+    private fun openFragment(fragment: Fragment, stackName: String?) {
+        if (!fragment.isAdded)
         supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment, fragment)
-            .addToBackStack("stack")
-            .commit()
+                .beginTransaction()
+                .replace(R.id.fragment, fragment)
+                .addToBackStack(stackName)
+                .commit()
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        supportFragmentManager.popBackStack("stack", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+
 }
